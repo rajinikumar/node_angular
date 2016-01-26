2fb8f530f05b83448d0ede66ed292b8d5de2b253
Rajini Kumar <rajinikumar.r@bambeeq.com>
2016-01-26 20:38:50 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,3 @@
+//var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+var connectionString = "postgres://postgres:rajini@localhost:/myapps_angular";
+module.exports = connectionString;
